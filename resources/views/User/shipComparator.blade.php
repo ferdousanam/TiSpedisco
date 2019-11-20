329b31f1fe3b5a8c5b896612b6f91956d45ddf0e
Ferdous Anam <ferdous.anam@gmail.com>
2019-11-20 15:09:03 +0600

    Merged Continue

--- resources/views/User/shipComparator.blade.php
+++ resources/views/User/shipComparator.blade.php
@@ -114,7 +114,6 @@
                                     </div>
 
                                 </div>
-
                             @endif
                         @endforeach
                     </div>
